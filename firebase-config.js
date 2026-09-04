fca2300d09cdb4252d4118fb8cc7980b7faa7578
triplexpublicity-cell <triplexpublicity@gmail.com>
2026-09-05 00:13:42 +0530

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// Replace the values below with the firebaseConfig shown in Firebase Console.
+// Do NOT put your RailRadar secret key here.
+window.TRACK_TRAIN_FIREBASE_CONFIG = {
+  apiKey: "YOUR_FIREBASE_API_KEY",
+  authDomain: "YOUR_PROJECT.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT.firebasestorage.app",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  appId: "YOUR_FIREBASE_APP_ID"
+};
