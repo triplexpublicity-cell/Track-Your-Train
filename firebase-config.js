4eede8f22ee764cf091d45bfedc88505e2b3ae9a
triplexpublicity-cell <triplexpublicity@gmail.com>
2026-09-05 00:33:56 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,8 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
+window.TRACK_TRAIN_FIREBASE_CONFIG = {
   apiKey: "AIzaSyAJQA-4BMrYJNkKF951XFrD5JHS7BkH224",
   authDomain: "track-your-train-1177f.firebaseapp.com",
   projectId: "track-your-train-1177f",
   storageBucket: "track-your-train-1177f.firebasestorage.app",
   messagingSenderId: "99335702047",
-  appId: "1:99335702047:web:0e451ef4fab522313db72a",
-  measurementId: "G-302JC7MVES"
+  appId: "1:99335702047:web:0e451ef4fab522313db72a"
 };
