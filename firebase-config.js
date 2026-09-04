7cd221bd6a166cd2c004127cb80a764fdd555a6b
triplexpublicity-cell <triplexpublicity@gmail.com>
2026-09-05 01:35:54 +0530

    Delete firebase-config.js

--- firebase-config.js
+++
@@ -1,8 +0,0 @@
-window.TRACK_TRAIN_FIREBASE_CONFIG = {
-  apiKey: "AIzaSyAJQA-4BMrYJNkKF951XFrD5JHS7BkH224",
-  authDomain: "track-your-train-1177f.firebaseapp.com",
-  projectId: "track-your-train-1177f",
-  storageBucket: "track-your-train-1177f.firebasestorage.app",
-  messagingSenderId: "99335702047",
-  appId: "1:99335702047:web:0e451ef4fab522313db72a"
-};
